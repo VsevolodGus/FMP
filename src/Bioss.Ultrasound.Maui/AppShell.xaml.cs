96ea9a6e59992ab8468b181b05f31e0a3e2675de
kolchevskaya <kolchevskayaam@gmail.com>
2026-02-28 01:33:24 +0300

    окраска tabbar элементов

--- src/Bioss.Ultrasound.Maui/AppShell.xaml.cs
+++ src/Bioss.Ultrasound.Maui/AppShell.xaml.cs
@@ -5,12 +5,15 @@ namespace Bioss.Ultrasound.Maui;
 
 public partial class AppShell : Shell
 {
+    private const string tabBarItemColor = "#4E86E6";
     public AppShell(IServiceProvider services)
     {
         InitializeComponent();
 
-        // TabBar снизу
         var tabBar = new TabBar();
+
+        SetTabBarForegroundColor(tabBar, Color.FromArgb(tabBarItemColor));
+        SetTabBarTitleColor(tabBar, Color.FromArgb(tabBarItemColor));
 
         tabBar.Items.Add(CreateTab<MainPage>(services, AppStrings.Main_Title, "ic_heart_pulse.svg"));
         tabBar.Items.Add(CreateTab<RecordsPage>(services, AppStrings.Records_Title, "ic_records.svg"));
@@ -25,7 +28,6 @@ public partial class AppShell : Shell
         {
             Title = title,
             Icon = ImageSource.FromFile(iconFile),
-
             // ВАЖНО: фабрика создаёт страницу через DI, поэтому конструкторы с параметрами работают
             ContentTemplate = new DataTemplate(services.GetRequiredService<T>)
         };
